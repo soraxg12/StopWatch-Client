001aa4b5060e45305ffecf19e78e810d99e6fb55
soraxg12 <contadevervidio@gmail.com>
2021-06-14 16:01:24 -0300

    updating the index

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {StopWatch} from './pages/stopwatch';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StopWatch />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
